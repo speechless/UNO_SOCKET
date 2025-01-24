2e89c57a4cf642fc2bda59b9832e8e3c867c1590
Speechless <overexytb@gmail.com>
2025-01-24 15:00:37 +0100

    Structures v1

---
+++ struct.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef enum{
+    ROUGE,
+    BLEU,
+    JAUNE,
+    VERT
+} Couleur;
+
+typedef enum{
+    ZERO,
+    UN,
+    DEUX,
+    TROIS,
+    QUATRE,
+    CINQ,
+    SIX,
+    SEPT,
+    HUIT,
+    NEUF,
+    PLUS_DEUX,
+    PLUS_QUATRE,
+    PASSE_TOUR,
+    CHANGEMENT_SENS,
+    CHANGEMENT_COULEUR
+} Valeur;
+
+typedef struct 
+{
+    enum Couleur Couleur;
+    enum Valeur Valeur;
+} Carte;
+
+typedef struct {
+    
+} Joueur;
+
