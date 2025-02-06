843d0e7f13fc299c3ab168ae8e398eb49765c621
t-vanderb <thom4.vdb@gmail.com>
2025-02-06 18:45:11 +0100

    Commentaires doxy

--- includes/requetes.h
+++ includes/requetes.h
@@ -72,26 +72,121 @@ typedef struct {
 	int code;
 } rejoindre_partie_t;
 
-
-
+/**
+ * Sérialise les données de base
+ * @param data Données à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserData(basic_data_t* data, char* chaine);
+
+/**
+ * Désérialise les données de base
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param data Données résultantes
+ */
 void deserialiserData(char* chaine, basic_data_t* data);
+
+/**
+ * Envoie un message d'erreur
+ * @param socket Socket à utiliser pour l'envoi
+ * @param message Message d'erreur à envoyer
+ */
 void envoyerErreur(socket_t socket, char* message);
 
+/**
+ * Sérialise un salon
+ * @param salon Salon à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserSalon(salon_t* salon, char* chaine);
+
+/**
+ * Désérialise un salon
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param salon Salon résultant
+ */
 void deserialiserSalon(char* chaine, salon_t* salon);
-void initSalon(salon_t* salon, int id, int isPrivate, int isHost, char* adresseHost, short portHost, int nbJoueursActuels, int nbJoueursMax);
+
+/**
+ * Initialise un salon
+ * @param salon Salon à initialiser
+ * @param id Identifiant du salon
+ * @param isPrivate 1 si le salon est privé, 0 sinon
+ * @param idHost Identifiant de l'hôte du salon
+ * @param adresseHost Adresse de l'hôte du salon
+ * @param portHost Port de l'hôte du salon
+ * @param nbJoueursActuels Nombre de joueurs actuels dans le salon
+ * @param nbJoueursMax Nombre de joueurs maximum dans le salon
+ */
+void initSalon(salon_t* salon, int id, int isPrivate, int idHost, char* adresseHost, short portHost, int nbJoueursActuels, int nbJoueursMax);
+
+/**
+ * Envoie les informations d'un salon
+ * @param socket Socket à utiliser pour l'envoi
+ * @param salon Salon à envoyer
+ */
 void envoyerSalon(socket_t socket, salon_t salon);
+
+/**
+ * Affiche les informations d'un salon
+ * @param salon Salon à afficher
+ */
 void afficherSalon(salon_t salon);
 
+/**
+ * Sérialise une demande de création de partie
+ * @param demande Demande à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserCreationPartie(creation_partie_t* demande, char* chaine);
+
+/**
+ * Désérialise une demande de création de partie
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param demande Demande résultante
+ */
 void deserialiserCreationPartie(char* chaine, creation_partie_t* demande);
+
+/**
+ * Envoie une demande de création de partie
+ * @param socket Socket à utiliser pour l'envoi
+ * @param demande Demande à envoyer
+ */
 void envoyerCreationPartie(socket_t socket, creation_partie_t demande);
 
+/**
+ * Sérialise une demande de rejoindre une partie
+ * @param demande Demande à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserRejoindrePartie(rejoindre_partie_t* demande, char* chaine);
+
+/**
+ * Désérialise une demande de rejoindre une partie
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param demande Demande résultante
+ */
 void deserialiserRejoindrePartie(char* chaine, rejoindre_partie_t* demande);
+
+/**
+ * Envoie une demande de rejoindre une partie
+ * @param socket Socket à utiliser pour l'envoi
+ * @param demande Demande à envoyer
+ */
 void envoyerRejoindrePartie(socket_t socket, rejoindre_partie_t demande);
 
+/**
+ * Sérialise un client
+ * @param client Client à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserClient(client_t* client, char* chaine);
+
+/**
+ * Désérialise un client
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param client Client résultant
+ */
 void deserialiserClient(char* chaine, client_t* client);
+
 #endif
--- src/requetes.c
+++ src/requetes.c
@@ -3,17 +3,32 @@
 #include <data.h>
 #include <string.h>
 
+/**
+ * Sérialise les données de base
+ * @param data Données à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserData(basic_data_t* data, char* chaine) {
 	sprintf(chaine, "[%d] : %s", data->code, data->data);
 	//fprintf(stderr, "envoi : #%s#\n", (char*)chaine);
 }
 
+/**
+ * Désérialise les données de base
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param data Données résultantes
+ */
 void deserialiserData(char* chaine, basic_data_t* data) {
 	//fprintf(stderr, "Reçu : #%s#\n", (char*)chaine);
 	sscanf(chaine, "[%d] : %[\001-\377]", &data->code, data->data);
 	//fprintf(stderr, "Désérialisé : #[%d] : %s#\n", data->code, data->data);
 }
 
+/**
+ * Envoie un message d'erreur
+ * @param socket Socket à utiliser pour l'envoi
+ * @param message Message d'erreur à envoyer
+ */
 void envoyerErreur(socket_t socket, char* message) {
 	basic_data_t requete;
 
@@ -25,7 +40,11 @@ void envoyerErreur(socket_t socket, char* message) {
 	envoyer(socket, &requete, (pFct)serialiserData);
 }
 
-
+/**
+ * Sérialise un salon
+ * @param salon Salon à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserSalon(salon_t* salon, char* chaine) {
 	char buffer[50];
 	afficherSalon(*salon);
@@ -40,6 +59,11 @@ void serialiserSalon(salon_t* salon, char* chaine) {
 	fprintf(stderr, "envoi : #%s#\n", (char*)chaine);
 }
 
+/**
+ * Désérialise un salon
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param salon Salon résultant
+ */
 void deserialiserSalon(char* chaine, salon_t* salon) {
 	fprintf(stderr, "recu : #%s#\n", (char*)chaine);
 	char chaineIdJoueurs[20];
@@ -57,14 +81,14 @@ void deserialiserSalon(char* chaine, salon_t* salon) {
 
 /**
  * Initialise un salon
- * @param salon : le salon à initialiser
- * @param id : l'identifiant du salon
- * @param isPrivate : 1 si le salon est privé, 0 sinon
- * @param isHost : 1 si le client est l'hôte du salon, 0 sinon
- * @param adresseHost : l'adresse de l'hôte du salon
- * @param portHost : le port de l'hôte du salon
- * @param nbJoueursActuels : le nombre de joueurs actuels dans le salon
- * @param nbJoueursMax : le nombre de joueurs maximum dans le salon
+ * @param salon Salon à initialiser
+ * @param id Identifiant du salon
+ * @param isPrivate 1 si le salon est privé, 0 sinon
+ * @param idHost Identifiant de l'hôte du salon
+ * @param adresseHost Adresse de l'hôte du salon
+ * @param portHost Port de l'hôte du salon
+ * @param nbJoueursActuels Nombre de joueurs actuels dans le salon
+ * @param nbJoueursMax Nombre de joueurs maximum dans le salon
  */
 void initSalon(salon_t* salon, int id, int isPrivate, int idHost, char* adresseHost, short portHost, int nbJoueursActuels, int nbJoueursMax) {
 	salon->id = id;
@@ -76,6 +100,11 @@ void initSalon(salon_t* salon, int id, int isPrivate, int idHost, char* adresseH
 	salon->nbJoueursMax = nbJoueursMax;
 }
 
+/**
+ * Envoie les informations d'un salon
+ * @param socket Socket à utiliser pour l'envoi
+ * @param salon Salon à envoyer
+ */
 void envoyerSalon(socket_t socket, salon_t salon) {
 	basic_data_t requete;
 
@@ -87,6 +116,10 @@ void envoyerSalon(socket_t socket, salon_t salon) {
 	envoyer(socket, &requete, (pFct)serialiserData);
 }
 
+/**
+ * Affiche les informations d'un salon
+ * @param salon Salon à afficher
+ */
 void afficherSalon(salon_t salon) {
 	printf("Salon.id = %d\n", salon.id);
 	printf("Salon.isPrivate = %d\n", salon.isPrivate);
@@ -103,17 +136,31 @@ void afficherSalon(salon_t salon) {
 	printf("Salon.code = %d\n", salon.code);
 }
 
-
+/**
+ * Sérialise une demande de création de partie
+ * @param demande Demande à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserCreationPartie(creation_partie_t* demande, char* chaine) {
 	sprintf(chaine, "%d:%d:%d:%s:%d", demande->idClient, demande->isPrivate, demande->nbJoueursMax, demande->adresseHost, demande->portHost);
 	//fprintf(stderr, "envoi : #%s#\n", (char*)chaine);
 }
 
+/**
+ * Désérialise une demande de création de partie
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param demande Demande résultante
+ */
 void deserialiserCreationPartie(char* chaine, creation_partie_t* demande) {
 	//fprintf(stderr, "Reçu : #%s#\n", (char*)chaine);
 	sscanf(chaine, "%d:%d:%d:%[^:]:%hd", &demande->idClient, &demande->isPrivate, &demande->nbJoueursMax, demande->adresseHost, &demande->portHost);
 }
 
+/**
+ * Envoie une demande de création de partie
+ * @param socket Socket à utiliser pour l'envoi
+ * @param demande Demande à envoyer
+ */
 void envoyerCreationPartie(socket_t socket, creation_partie_t demande) {
 	basic_data_t requete;
 
@@ -125,16 +172,31 @@ void envoyerCreationPartie(socket_t socket, creation_partie_t demande) {
 	envoyer(socket, &requete, (pFct)serialiserData);
 }
 
+/**
+ * Sérialise une demande de rejoindre une partie
+ * @param demande Demande à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserRejoindrePartie(rejoindre_partie_t* demande, char* chaine) {
 	sprintf(chaine, "%d:%d:%d", demande->idClient, demande->isPrivate, demande->code);
 	//fprintf(stderr, "envoi : #%s#\n", (char*)chaine);
 }
 
+/**
+ * Désérialise une demande de rejoindre une partie
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param demande Demande résultante
+ */
 void deserialiserRejoindrePartie(char* chaine, rejoindre_partie_t* demande) {
 	//fprintf(stderr, "Reçu : #%s#\n", (char*)chaine);
 	sscanf(chaine, "%d:%d:%d", &demande->idClient, &demande->isPrivate, &demande->code);
 }
 
+/**
+ * Envoie une demande de rejoindre une partie
+ * @param socket Socket à utiliser pour l'envoi
+ * @param demande Demande à envoyer
+ */
 void envoyerRejoindrePartie(socket_t socket, rejoindre_partie_t demande) {
 	basic_data_t requete;
 
@@ -146,12 +208,21 @@ void envoyerRejoindrePartie(socket_t socket, rejoindre_partie_t demande) {
 	envoyer(socket, &requete, (pFct)serialiserData);
 }
 
-
+/**
+ * Sérialise un client
+ * @param client Client à sérialiser
+ * @param chaine Chaîne de caractères résultante
+ */
 void serialiserClient(client_t* client, char* chaine) {
 	sprintf(chaine, "%d:%s:%d", client->id, client->adresse, client->port);
 	//fprintf(stderr, "envoi : #%s#\n", (char*)chaine);
 }
 
+/**
+ * Désérialise un client
+ * @param chaine Chaîne de caractères à désérialiser
+ * @param client Client résultant
+ */
 void deserialiserClient(char* chaine, client_t* client) {
 	//fprintf(stderr, "Reçu : #%s#\n", (char*)chaine);
 	sscanf(chaine, "%d:%[^:]:%hd", &client->id, client->adresse, &client->port);
